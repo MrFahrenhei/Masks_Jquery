7640be749f16c3981c8aeb1803feaff2c9a44cc1
Vinícius V. Beraldo <80232541+MrFahrenhei@users.noreply.github.com>
2022-05-31 11:50:57 -0300

    Add files via upload

--- masks.js
+++ masks.js
@@ -41,9 +41,9 @@ jQuery(document).ready(function($){
       $('.crazy_cep').mask(mask, options);
     }});
   
-    $('#cnpj').mask('00.000.000/0000-00', {reverse: true});
-    $('#cpf').mask('000.000.000-00', {reverse: true});
-    $('#money').mask('#.##0,00', {reverse: true});
+    $('.cnpj').mask('00.000.000/0000-00', {reverse: true});
+    $('.cpf').mask('000.000.000-00', {reverse: true});
+    $('.money').mask('#.##0,00', {reverse: true});
   
     var SPMaskBehavior = function (val) {
       return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
